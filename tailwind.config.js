da55340ad091591698fd03d8dc40c09c2dfbafb5
Xomara <xomaraweb@gmail.com>
2025-06-09 17:16:31 -0500

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
